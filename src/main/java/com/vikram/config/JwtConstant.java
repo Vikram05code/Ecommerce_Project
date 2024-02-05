c8d2a01fddbb0231ef7ec096f81767d83b052bbe
Vikram <vikky5vikram@gmail.com>
2024-02-05 23:33:49 +0530

    Implemented Spring Security Using JWT

---
+++ src/main/java/com/vikram/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.vikram.config;
+
+public class JwtConstant {
+
+	public static final String SECERET_KEY="jhjggdhdchbdchhbdcbjytrdfghjklmnbvc";
+	public static final String JWT_HEADER="Authorization";
+}
