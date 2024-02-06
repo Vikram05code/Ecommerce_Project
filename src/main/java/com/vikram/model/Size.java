916f59ca5f6a007c62d4e48abaf109a38a16fbd6
Vikram <vikky5vikram@gmail.com>
2024-02-06 14:23:44 +0530

    Created All the Model Classes

---
+++ src/main/java/com/vikram/model/Size.java
@@ -0,0 +1,18 @@
+package com.vikram.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Size {
+
+	private String name;
+	
+	private int quantity;
+	
+}
