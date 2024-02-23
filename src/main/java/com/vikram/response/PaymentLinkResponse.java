7e41b111d8b85cb0cf9f3a1c436f84577ac32212
Vikram <vikky5vikram@gmail.com>
2024-02-23 17:47:04 +0530

    Committing code for creating PaymentLink Method for PaymentController

---
+++ src/main/java/com/vikram/response/PaymentLinkResponse.java
@@ -0,0 +1,18 @@
+package com.vikram.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentLinkResponse {
+
+	private String payment_link_url;
+	private String payment_link_id;
+	
+	
+}
